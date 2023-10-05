97332e114cfe795edf498e89dbca39895d7be443
Jacob Foote <jacobjfoote01@gmail.com>
2023-10-05 15:59:56 -0600

    commit

---
+++ ajax-exercise.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+// PART 1: Show Dog Photo
+
+async function showDogPhoto(evt) {
+  const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+  const imgUrl = response.data.message
+  document.querySelector("#dog-image").innerHTML = `<img src=${imgUrl}>`
+ }
+
+ document.querySelector('#get-dog-image').addEventListener('click', showDogPhoto);
+
+// PART 2: Show Weather
+
+async function showWeather(evt) {
+  const zipcode = document.querySelector('#zipcode-field').value
+  const url =`/weather.txt?zipcode=${zipcode}`
+  const response = await axios.get(url)
+  document.querySelector("#weather-info").innerText = response.data
+}
+
+document.querySelector('#weather-button').addEventListener('click', showWeather);
+
+// PART 3: Order Cookies
+
+async function orderCookies(evt) {
+   evt.preventDefault() 
+   
+   const cookieType = document.querySelector('#cookie-type-field').value
+   const qty = document.querySelector('#qty-field')
+   const response = await axios.post(
+    '/order-cookies.json',
+    {cookieType: cookieType, qty: qty}
+   )
+   
+   const orderStatusDiv = document.querySelector("#order-status")
+   orderStatusDiv.innerText = response.data.message
+   if (response.data.resultCode === "ERROR") {
+    orderStatusDiv.classList.add('order-error')
+   } else {
+    orderStatusDiv.classList.remove('order-error')
+   }
+}
+document.querySelector('#order-form').addEventListener('submit', orderCookies);
+
+// PART 4: iTunes Search
+
+function iTunesSearch(evt) {
+  evt.preventDefault();
+  const searchTerm = document.querySelector("#search-term").value;
+
+  // TODO: In the #itunes-results list, show all results in the following format:
+  // `Artist: ${artistName} Song: ${trackName}`
+}
+document.querySelector('#itunes-search-form').addEventListener('submit', iTunesSearch);
